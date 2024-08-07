8a9e34235e231c21a72c51fd1833d41944eb8cb6
Dmitry Sorokin <103883634+Memassik@users.noreply.github.com>
2024-08-07 18:33:41 +0300

    Switched To LazyVim

--- init.lua
+++ init.lua
@@ -1,24 +1,2 @@
---core nvim config
-require("memassik.core.configs")
-require("memassik.core.mappings")
-require("memassik.plugins-setup")
-require("memassik.core.colorscheme")
-
---plugins
---require("memassik.plugins.impatient")
-require("memassik.plugins.neotree")
-require("memassik.plugins.telescope")
-require("memassik.plugins.treesitter")
-require("memassik.plugins.lualine")
-
-require("memassik.plugins.nvim-cmp")
-require("memassik.plugins.nvim-lint")
-require("memassik.plugins.nvim-formatter")
-require("memassik.plugins.comment")
---require("memassik.plugins.barbar")
-require("memassik.plugins.toggleterm")
-require("memassik.plugins.todo")
-require("memassik.plugins.nvim-dap")
-require("memassik.plugins.cmakeTools")
-require("memassik.plugins.ibl")
-require("memassik.plugins.aerial")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/memassik/plugins/toggleterm.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.keymap -- for conciseness
---terminal bindings
-
-keymap.set("n", "<leader>tt", ":ToggleTerm direction=float<CR>i")
-keymap.set("t", "<Esc>", [[<C-\><C-n>:ToggleTerm<CR>]])
